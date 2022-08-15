06a4667134034f976d6cdc008fb802414dd85d1d
Xeno <xenotactic.game@gmail.com>
2022-08-15 16:59:35 -0700

    Cleaning up build stuff pt. 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
         google()
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
         maven { url = uri("https://plugins.gradle.org/m2/") }
+        println("Finish setting up repositories for buildscript.")
     }
 }
 
@@ -16,18 +17,4 @@ plugins {
     kotlin("plugin.serialization") apply false
 
     println("Finished setting up plugins")
-}
-
-allprojects {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        google()
-        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
-        maven { url = uri("https://plugins.gradle.org/m2/") }
-    }
-}
-
-subprojects {
-    println("Going through subprojects, name: ${this.name}")
 }
